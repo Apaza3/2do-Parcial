013984655fb59d6c0b26d82453474bbd52fba429
aaron <www.agentepotasio@gmail.com>
2025-05-03 10:18:05 -0400

    Template Note Page creado

---
+++ src/componentes/templates/NotesPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import NoteCard from '../organisms/NoteCard';
+
+function NotesPage() {
+  return (
+    <div className="notes-page">
+      <NoteCard
+        title="Agenda/Topic"
+        code="SAAS-0000"
+        description="Description of what is done here"
+        date="3/8/2020"
+        user="Daniel Catari (dani O_o)"
+        userColor="orange"
+      />
+    </div>
+  );
+}
+
+export default NotesPage;
